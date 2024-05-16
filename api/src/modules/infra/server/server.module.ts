a2d2c9865ca17e67d9984ead1291afa7225dbf26
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 15:50:00 -0300

    [ :syringe: ] | create: module - server (module::infra)

---
+++ api/src/modules/infra/server/server.module.ts
@@ -0,0 +1,15 @@
+import { Container } from 'inversify';
+import { SERVER_REGISTRY } from './server.registry';
+import { EXPRESS_SERVER } from './htpp/express/express.server';
+import { CORS } from './htpp/express/cors';
+import { BODY_PARSER } from './htpp/express/body.parser';
+
+export const SERVER_MODULE = new Container({ autoBindInjectable: true });
+
+SERVER_MODULE.bind(SERVER_REGISTRY.HTTP.EXPRESS).toConstantValue(
+  EXPRESS_SERVER,
+);
+SERVER_MODULE.bind(SERVER_REGISTRY.HTTP.CORS).toConstantValue(CORS);
+SERVER_MODULE.bind(SERVER_REGISTRY.HTTP.PARSER.BODY).toConstantValue(
+  BODY_PARSER,
+);
