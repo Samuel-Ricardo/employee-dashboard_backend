3e1488e6de2d942bc50312037f2a2327f9fb37ad
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 16:55:29 -0300

    [ :syringe: ] | update: factory - app > router (app::module)

--- api/src/modules/app.factory.ts
+++ api/src/modules/app.factory.ts
@@ -1,5 +1,7 @@
+import { ROUTER_FACTORY } from './application/router/router.factory';
 import { INFRA_FACTORY } from './infra/infra.factory';
 
 export const MODULES = {
   INFRA: INFRA_FACTORY,
+  ROUTER: ROUTER_FACTORY,
 };
