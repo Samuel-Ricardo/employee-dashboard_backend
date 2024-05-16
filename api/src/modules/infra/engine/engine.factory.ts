122f64b2963b6c019985ce719742ed6724c3e0f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 20:36:12 -0300

    [ :syringe: ] | update: factory - engine > mongoose (module::infra)

--- api/src/modules/infra/engine/engine.factory.ts
+++ api/src/modules/infra/engine/engine.factory.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import { ENGINE_MODULE } from './engine.module';
 import { ENGINE_REGISTRY } from './engine.registry';
+import { MONGOOSE_TYPE } from '@/@types/module/infra/engine/database/mongodb/mongoose.type';
 
 export const ENGINE_FACTORY = {
   ROUTER: {
     HTTP: {
       EXPRESS: () =>
         ENGINE_MODULE.get<Router>(ENGINE_REGISTRY.ROUTER.HTTP.EXPRESS),
+    },
+  },
+  DATABASE: {
+    MONGODB: {
+      MONGOOSE: () =>
+        ENGINE_MODULE.get<MONGOOSE_TYPE>(
+          ENGINE_REGISTRY.DATABASE.MONGODB.MONGOOSE,
+        ),
     },
   },
 };
