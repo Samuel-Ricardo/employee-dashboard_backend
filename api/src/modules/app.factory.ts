125ea935b00cf3e629d6ac0433a11ee388eac8da
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 16:59:20 -0300

    [ :syringe: ] | create: module - application (app::module)

--- api/src/modules/app.factory.ts
+++ api/src/modules/app.factory.ts
@@ -1,7 +1,8 @@
+import { APPLICATION_FACTORY } from './application/application.factory';
 import { ROUTER_FACTORY } from './application/router/router.factory';
 import { INFRA_FACTORY } from './infra/infra.factory';
 
 export const MODULES = {
   INFRA: INFRA_FACTORY,
-  ROUTER: ROUTER_FACTORY,
+  APPLICATION: APPLICATION_FACTORY,
 };
--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,7 +1,12 @@
 import { Container } from 'inversify';
 import { INFRA_MODULE } from './infra/infra.module';
 import { ROUTER_MODULE } from './application/router/router.module';
+import { APPLICATION_MODULE } from './application/application.module';
 
 const _MODULE = new Container({ autoBindInjectable: true });
 
-export const APP_MODULE = Container.merge(_MODULE, INFRA_MODULE, ROUTER_MODULE);
+export const APP_MODULE = Container.merge(
+  _MODULE,
+  INFRA_MODULE,
+  APPLICATION_MODULE,
+);
