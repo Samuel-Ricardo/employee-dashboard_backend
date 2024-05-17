407a9ff5d5e0e0e3189efcef003ac50765efa4c1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 22:54:46 -0300

    [ :video_game: ] | create: controller - update > employee (module::application)

--- api/src/modules/application/controller/employee/employee.controller.ts
+++ api/src/modules/application/controller/employee/employee.controller.ts
@@ -1,4 +1,5 @@
 import { ICreateEmployeeDTO } from '@/modules/domain/DTO/employee/create.dto';
+import { IUpdateEmployeeDTO } from '@/modules/domain/DTO/employee/update.dto';
 import { IEmployeeService } from '@/modules/domain/service/employee.service';
 import { injectable } from 'inversify';
 
@@ -9,5 +10,9 @@ export class EmployeeController {
   async create(DTO: ICreateEmployeeDTO) {
     const result = await this._service.create(DTO);
     return { result };
+  }
+
+  async update(DTO: IUpdateEmployeeDTO) {
+    await this._service.update(DTO);
   }
 }
