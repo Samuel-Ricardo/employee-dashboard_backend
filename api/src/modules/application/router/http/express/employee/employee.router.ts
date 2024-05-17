cc7a72b1dcbacdc6b4a188022240cb6226bd0b69
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 23:13:15 -0300

    [ :vertical_traffic_light: ] | create: router - employee (app::module::express)

---
+++ api/src/modules/application/router/http/express/employee/employee.router.ts
@@ -0,0 +1,52 @@
+import { MODULE } from '@/modules/app.registry';
+import { EmployeeController } from '@/modules/application/controller/employee/employee.controller';
+import { TRY } from '@/util/try.util';
+import { Router } from 'express';
+import { interfaces } from 'inversify';
+
+export const EXPRESS_USER_ROUTER = ({ container }: interfaces.Context) => {
+  const APP_ROUTER = container.get<Router>(
+    MODULE.INFRA.ENGINE.ROUTER.HTTP.EXPRESS,
+  );
+
+  const EMPLOYEE = container.get<EmployeeController>(
+    MODULE.APPLICATION.CONTROLLER.EMPLOYEE,
+  );
+
+  APP_ROUTER.get('/employee/', (req, res, next) =>
+    TRY(async () => res.status(200).send(await EMPLOYEE.findAll()), next),
+  );
+
+  APP_ROUTER.get('/employee/:id', (req, res, next) =>
+    TRY(
+      async () => res.status(200).send(await EMPLOYEE.findOne(req.params)),
+      next,
+    ),
+  );
+
+  APP_ROUTER.post('/employee/', (req, res, next) =>
+    TRY(
+      async () => res.status(201).send(await EMPLOYEE.create(req.body)),
+      next,
+    ),
+  );
+
+  APP_ROUTER.put('/employee/:id', (req, res, next) =>
+    TRY(
+      async () =>
+        res
+          .status(204)
+          .send(await EMPLOYEE.update({ ...req.params, ...req.body })),
+      next,
+    ),
+  );
+
+  APP_ROUTER.delete('/employee/:id', (req, res, next) =>
+    TRY(
+      async () => res.status(204).send(await EMPLOYEE.delete(req.params)),
+      next,
+    ),
+  );
+
+  return APP_ROUTER;
+};
