f22b7521a52f8b56ce0319a3c186af7b38172577
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 22:14:31 -0300

    [ :sparkles: ] | create: use-case - update > employee (module::application)

---
+++ api/src/modules/application/use_case/employee/update.use_case.ts
@@ -0,0 +1,10 @@
+import { injectable } from 'inversify';
+import { MongooseEmployeeRpositorySupport } from '../../support/repository/mongoose/employee.support';
+import { IUpdateEmployeeDTO } from '@/modules/domain/DTO/employee/update.dto';
+
+@injectable()
+export class UpdateEmployeeUseCase extends MongooseEmployeeRpositorySupport {
+  async execute(DTO: IUpdateEmployeeDTO) {
+    return await this._repository.update(DTO);
+  }
+}
