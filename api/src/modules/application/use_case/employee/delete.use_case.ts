5d138f6cb53e7d95b4ac2471670f80dffb6ff2c9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 22:15:05 -0300

    [ :sparkles: ] | create: use-case - delete > employee (module::application)

---
+++ api/src/modules/application/use_case/employee/delete.use_case.ts
@@ -0,0 +1,10 @@
+import { injectable } from 'inversify';
+import { MongooseEmployeeRpositorySupport } from '../../support/repository/mongoose/employee.support';
+import { IDeleteEmployeeDTO } from '@/modules/domain/DTO/employee/delete,dto';
+
+@injectable()
+export class DeleteEmployeeUseCase extends MongooseEmployeeRpositorySupport {
+  async execute(DTO: IDeleteEmployeeDTO) {
+    return await this._repository.delete(DTO);
+  }
+}
