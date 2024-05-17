3d24216ec4f5d7416ce5d5eba5c493eed45b45e5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-16 23:03:54 -0300

    [ :syringe: ] | update: module - application > controller (app::module)

--- api/src/modules/application/application.module.ts
+++ api/src/modules/application/application.module.ts
@@ -4,6 +4,7 @@ import { MIDDLEWARE_MODULE } from './middleware/middleware.module';
 import { REPOSITORY_MODEL } from './repository/repository.module';
 import { USE_CASE_MODULE } from './use_case/use_case.module';
 import { SERVICE_MODULE } from './service/service.module';
+import { CONTROLLER_MODULE } from './controller/controller.module';
 
 const _MODULE = new Container({ autoBindInjectable: true });
 
@@ -14,4 +15,5 @@ export const APPLICATION_MODULE = Container.merge(
   REPOSITORY_MODEL,
   USE_CASE_MODULE,
   SERVICE_MODULE,
+  CONTROLLER_MODULE,
 );
