e95542ca4cf3593ee84832d50efd22d0e0c38d97
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 09:52:03 -0300

    [ :safety_vest: ] | create: validation - exployee  (module::application)

---
+++ api/src/modules/application/validation/zod/employee/employee.validation.ts
@@ -0,0 +1,17 @@
+import { catchZod } from '@/util/validation/zod/catch.util';
+import { z } from 'zod';
+
+export const EmployeeSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+    name: z.string().catch(catchZod),
+    role: z.string().catch(catchZod),
+    department: z.string().catch(catchZod),
+    admission_date: z
+      .date()
+      .or(z.string().date('invalid date'))
+      .catch(catchZod),
+  })
+  .catch(catchZod);
+
+export type EmployeeValidationData = z.infer<typeof EmployeeSchema>;
