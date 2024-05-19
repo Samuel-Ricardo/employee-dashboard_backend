e72fd79c14b80259ac9c510e434d10b3f6161bf9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 11:02:23 -0300

    [ :goal_net: ] | update: router - employee > catch errors (app::module::express)

--- api/src/modules/application/router/http/express/employee/employee.router.ts
+++ api/src/modules/application/router/http/express/employee/employee.router.ts
@@ -3,8 +3,14 @@ import { EmployeeController } from '../../../../../application/controller/employ
 import { TRY } from '../../../../../../util/try.util';
 import { Router } from 'express';
 import { interfaces } from 'inversify';
+import { ZOD_VALIDATION_MIDDLEWARE_TYPE } from '../../../../../../@types/module/application/middleware/validation/zod.type';
+import { FindOneEmployeeSchema } from '../../../../validation/zod/employee/find/one.validation';
 
 export const EXPRESS_EMPLOYEE_ROUTER = ({ container }: interfaces.Context) => {
+  const VALIDATION_MIDDLEWARE = container.get<ZOD_VALIDATION_MIDDLEWARE_TYPE>(
+    MODULE.APPLICATION.MIDDLEWARE.VALIDATION.ZOD,
+  );
+
   const APP_ROUTER = container.get<Router>(
     MODULE.INFRA.ENGINE.ROUTER.HTTP.EXPRESS,
   );
@@ -13,40 +19,53 @@ export const EXPRESS_EMPLOYEE_ROUTER = ({ container }: interfaces.Context) => {
     MODULE.APPLICATION.CONTROLLER.EMPLOYEE,
   );
 
-  APP_ROUTER.get('/api/employees/', (req, res, next) =>
-    TRY(async () => res.status(200).send(await EMPLOYEE.findAll()), next),
+  APP_ROUTER.get('/api/employees/', async (req, res, next) => {
+    try {
+      return res.status(200).send(await EMPLOYEE.findAll());
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  APP_ROUTER.get(
+    '/api/employees/:id',
+    VALIDATION_MIDDLEWARE(FindOneEmployeeSchema),
+    async (req, res, next) => {
+      try {
+        return res
+          .status(200)
+          .send(await EMPLOYEE.findOne({ id: req.params.id }));
+      } catch (error) {
+        next(error);
+      }
+    },
   );
 
-  APP_ROUTER.get('/api/employees/:id', (req, res, next) =>
-    TRY(
-      async () => res.status(200).send(await EMPLOYEE.findOne(req.params)),
-      next,
-    ),
-  );
+  APP_ROUTER.post('/api/employees/', async (req, res, next) => {
+    try {
+      return res.status(201).send(await EMPLOYEE.create(req.body));
+    } catch (error) {
+      next(error);
+    }
+  });
 
-  APP_ROUTER.post('/api/employees/', (req, res, next) =>
-    TRY(
-      async () => res.status(201).send(await EMPLOYEE.create(req.body)),
-      next,
-    ),
-  );
+  APP_ROUTER.put('/api/employees/:id', async (req, res, next) => {
+    try {
+      return res
+        .status(204)
+        .send(await EMPLOYEE.update({ ...req.params, ...req.body }));
+    } catch (error) {
+      next(error);
+    }
+  });
 
-  APP_ROUTER.put('/api/employees/:id', (req, res, next) =>
-    TRY(
-      async () =>
-        res
-          .status(204)
-          .send(await EMPLOYEE.update({ ...req.params, ...req.body })),
-      next,
-    ),
-  );
-
-  APP_ROUTER.delete('/api/employees/:id', (req, res, next) =>
-    TRY(
-      async () => res.status(204).send(await EMPLOYEE.delete(req.params)),
-      next,
-    ),
-  );
+  APP_ROUTER.delete('/api/employees/:id', async (req, res, next) => {
+    try {
+      return res.status(204).send(await EMPLOYEE.delete(req.params));
+    } catch (error) {
+      next(error);
+    }
+  });
 
   return APP_ROUTER;
 };
