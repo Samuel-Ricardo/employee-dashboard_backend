ea356a3a68c02760ce64795273b6674dea43c308
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 09:58:34 -0300

    [ :label: ] | create: type - middleware > validation [zod] (@type/application)

---
+++ api/src/@types/module/application/middleware/validation/zod.type.ts
@@ -0,0 +1,3 @@
+import { ZOD_VALIDATION_MIDDLEWARE } from '@/modules/application/middleware/validation/zod/validation.middleware';
+
+export type ZOD_VALIDATION_MIDDLEWARE = typeof ZOD_VALIDATION_MIDDLEWARE;
