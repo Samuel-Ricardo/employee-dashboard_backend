591023f25bcf028810c24ae6485ce788c3baba1e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-18 22:06:17 -0300

    [ :syringe: ] | update: module - engine > docs (module::infra)

--- api/src/modules/infra/engine/engine.module.ts
+++ api/src/modules/infra/engine/engine.module.ts
@@ -2,11 +2,14 @@ import { Container } from 'inversify';
 import { ENGINE_REGISTRY } from './engine.registry';
 import { EXPRESS_ROUTER } from './router/http/express.router';
 import { MONGOOSE } from './database/mongodb/mongoose.engine';
+import { SWAGGER } from './documentation/swagger.engine';
 
 export const ENGINE_MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
+
+ENGINE_MODULE.bind(ENGINE_REGISTRY.DOCS.SWAGGER).toConstantValue(SWAGGER);
 
 ENGINE_MODULE.bind(ENGINE_REGISTRY.ROUTER.HTTP.EXPRESS).toConstantValue(
   EXPRESS_ROUTER,
