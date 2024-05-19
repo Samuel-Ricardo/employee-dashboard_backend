10849544c4ccf34e5e64f0f1fc9b65bf22e5eafa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 10:15:45 -0300

    [ :safety_vest: ] | update: middleware - validation > zod  (module::application)

--- api/src/modules/application/middleware/validation/zod/validation.middleware.ts
+++ api/src/modules/application/middleware/validation/zod/validation.middleware.ts
@@ -12,10 +12,18 @@ export const ZOD_VALIDATION_MIDDLEWARE =
         { params: req.params, body: req.body },
       );
 
-      if (Object.keys({ ...req.body }).length > 0)
+      /*      if (Object.keys({ ...req.body }).length > 0)
         return schema.parse(req.body);
       if (Object.keys({ ...req.params }).length > 0)
         return schema.parse(req.params);
+*/
+
+      if (
+        Object.keys({ ...req.body }).length > 0 ||
+        Object.keys({ ...req.params }).length > 0
+      ) {
+        return schema.parse({ ...req.body, ...req.params });
+      }
 
       throw new NoDataProvidedError('No data provided');
     } catch (e) {
