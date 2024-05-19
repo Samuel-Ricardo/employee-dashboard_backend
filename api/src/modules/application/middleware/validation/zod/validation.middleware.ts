cf2407deba614eaaccb42229fa72f2e3facd4877
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 09:55:29 -0300

    [ :safety_vest: ] | create: middleware - validation > zod  (module::application)

---
+++ api/src/modules/application/middleware/validation/zod/validation.middleware.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+import { NoDataProvidedError } from '@/lib/error/validation/no_data.error';
+import { logger } from '@/lib/logger.lib';
+
+export const validate =
+  (schema: z.ZodSchema) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      logger.info(
+        { context: 'VALIDATOR', message: 'Data:' },
+        { params: req.params, body: req.body },
+      );
+
+      if (Object.keys({ ...req.body }).length > 0)
+        return schema.parse(req.body);
+      if (Object.keys({ ...req.params }).length > 0)
+        return schema.parse(req.params);
+
+      throw new NoDataProvidedError('No data provided');
+    } catch (e) {
+      next(e);
+    } finally {
+      next();
+    }
+  };
