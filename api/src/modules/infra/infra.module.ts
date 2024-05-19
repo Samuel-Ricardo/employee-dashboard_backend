5eacaba55d6e239d9c2a746c238d7e2ce0d0d77d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-18 22:43:31 -0300

    [ :syringe: ] | update: module - infra > docs (app::module)

--- api/src/modules/infra/infra.module.ts
+++ api/src/modules/infra/infra.module.ts
@@ -2,9 +2,11 @@ import { Container } from 'inversify';
 import { SERVER_MODULE } from './server/server.module';
 import { ENGINE_MODULE } from './engine/engine.module';
 import { CONFIG_MODULE } from './config/config.module';
+import { DOCS_MODULE } from './documentation/docs.module';
 
 export const INFRA_MODULE = Container.merge(
   SERVER_MODULE,
   ENGINE_MODULE,
   CONFIG_MODULE,
+  DOCS_MODULE,
 );
