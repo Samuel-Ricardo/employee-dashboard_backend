b13fe2880877ebdb3dce1c61d7175cb5014b7e8a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 11:09:33 -0300

    [ :safety_vest: ] | update: validation - employee  (module::application)

--- api/src/modules/application/repository/mongoose/employee/MongooseEmployeeRepository.ts
+++ api/src/modules/application/repository/mongoose/employee/MongooseEmployeeRepository.ts
@@ -7,6 +7,7 @@ import { IUpdateEmployeeDTO } from '../../../../domain/DTO/employee/update.dto';
 import { Employee } from '../../../../domain/entity/employee/employee.entity';
 import { IEmployeeRepository } from '../../../../domain/repository/employee.repository';
 import { injectable } from 'inversify';
+import { MongoDBError } from '../../../../../lib/error/database/mongodb.error';
 
 @injectable()
 export class MongooseEmployeeRepository implements IEmployeeRepository {
@@ -15,43 +16,63 @@ export class MongooseEmployeeRepository implements IEmployeeRepository {
   }
 
   async create(DTO: ICreateEmployeeDTO) {
-    const employee = new EmployeeModel(DTO);
-    const { _id } = await employee.save();
-    return {
-      id: _id.toString(),
-    };
+    try {
+      const employee = new EmployeeModel(DTO);
+      const { _id } = await employee.save();
+      return {
+        id: _id.toString(),
+      };
+    } catch (err: any) {
+      throw new MongoDBError('Error on Create Employee', 500, err);
+    }
   }
 
   async update(DTO: IUpdateEmployeeDTO) {
-    await EmployeeModel.findByIdAndUpdate(DTO.id, DTO, { new: true });
+    try {
+      await EmployeeModel.findByIdAndUpdate(DTO.id, DTO, { new: true });
+    } catch (err: any) {
+      throw new MongoDBError('Error on Update Employee', 500, err);
+    }
   }
 
   async delete({ id }: IDeleteEmployeeDTO) {
-    await EmployeeModel.findByIdAndDelete(id);
+    try {
+      await EmployeeModel.findByIdAndDelete(id);
+    } catch (err: any) {
+      throw new MongoDBError('Error on Delete Employee', 500, err);
+    }
   }
   async findOne({ id }: IFindOneEmployeeDTO) {
-    const result = await EmployeeModel.findById(id);
-    if (!result) throw new Error('Employee not found');
+    try {
+      const result = await EmployeeModel.findById(id);
+      if (!result) throw new MongoDBError('Employee not found');
 
-    return Employee.fromDTO({
-      id: result._id.toString(),
-      name: result.name,
-      role: result.role,
-      department: result.department,
-      admission_date: result.admission_date,
-    });
+      return Employee.fromDTO({
+        id: result._id.toString(),
+        name: result.name,
+        role: result.role,
+        department: result.department,
+        admission_date: result.admission_date,
+      });
+    } catch (err: any) {
+      throw new MongoDBError('Error on FindOne Employee', 500, err);
+    }
   }
 
   async findAll() {
-    const result = await EmployeeModel.find();
-    return result.map((employee) =>
-      Employee.fromDTO({
-        id: employee._id.toString(),
-        name: employee.name,
-        role: employee.role,
-        department: employee.department,
-        admission_date: employee.admission_date,
-      }),
-    );
+    try {
+      const result = await EmployeeModel.find();
+      return result.map((employee) =>
+        Employee.fromDTO({
+          id: employee._id.toString(),
+          name: employee.name,
+          role: employee.role,
+          department: employee.department,
+          admission_date: employee.admission_date,
+        }),
+      );
+    } catch (err: any) {
+      throw new MongoDBError('Error on FindAll Employee', 500, err);
+    }
   }
 }
