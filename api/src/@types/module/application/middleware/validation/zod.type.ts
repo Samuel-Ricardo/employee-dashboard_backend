1f30d1d8c66a632c483a293716d5958d507496fb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 10:00:16 -0300

    [ :syringe: ] | update: factory - middleware > validation (module::middleware)

--- api/src/@types/module/application/middleware/validation/zod.type.ts
+++ api/src/@types/module/application/middleware/validation/zod.type.ts
@@ -1,3 +1,3 @@
 import { ZOD_VALIDATION_MIDDLEWARE } from '@/modules/application/middleware/validation/zod/validation.middleware';
 
-export type ZOD_VALIDATION_MIDDLEWARE = typeof ZOD_VALIDATION_MIDDLEWARE;
+export type ZOD_VALIDATION_MIDDLEWARE_TYPE = typeof ZOD_VALIDATION_MIDDLEWARE;
--- api/src/modules/application/middleware/middleware.factory.ts
+++ api/src/modules/application/middleware/middleware.factory.ts
@@ -1,8 +1,15 @@
 import { ErrorRequestHandler, RequestHandler } from 'express';
 import { MIDDLEWARE_MODULE } from './middleware.module';
 import { MIDDLEWARE_REGISTRY } from './middleware.registry';
+import { ZOD_VALIDATION_MIDDLEWARE_TYPE } from '@/@types/module/application/middleware/validation/zod.type';
 
 export const MIDDLEWARE_FACTORY = {
+  VALIDATION: {
+    ZOD: () =>
+      MIDDLEWARE_MODULE.get<ZOD_VALIDATION_MIDDLEWARE_TYPE>(
+        MIDDLEWARE_REGISTRY.VALIDATION.ZOD,
+      ),
+  },
   ERROR: () =>
     MIDDLEWARE_MODULE.get<ErrorRequestHandler>(MIDDLEWARE_REGISTRY.ERROR),
   LOGGER: {
