13eca1696875686ea840234d676a2fe87a832b77
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-19 11:05:07 -0300

    [ :safety_vest: ] | validate: router - employee (module::application::express)

--- api/src/modules/application/router/http/express/employee/employee.router.ts
+++ api/src/modules/application/router/http/express/employee/employee.router.ts
@@ -5,6 +5,9 @@ import { Router } from 'express';
 import { interfaces } from 'inversify';
 import { ZOD_VALIDATION_MIDDLEWARE_TYPE } from '../../../../../../@types/module/application/middleware/validation/zod.type';
 import { FindOneEmployeeSchema } from '../../../../validation/zod/employee/find/one.validation';
+import { CreateEmployeeSchema } from '@/modules/application/validation/zod/employee/create.validation';
+import { UpdateEmployeeSchema } from '@/modules/application/validation/zod/employee/update.validation';
+import { DeleteEmployeeSchema } from '@/modules/application/validation/zod/employee/delete.validation';
 
 export const EXPRESS_EMPLOYEE_ROUTER = ({ container }: interfaces.Context) => {
   const VALIDATION_MIDDLEWARE = container.get<ZOD_VALIDATION_MIDDLEWARE_TYPE>(
@@ -41,31 +44,45 @@ export const EXPRESS_EMPLOYEE_ROUTER = ({ container }: interfaces.Context) => {
     },
   );
 
-  APP_ROUTER.post('/api/employees/', async (req, res, next) => {
-    try {
-      return res.status(201).send(await EMPLOYEE.create(req.body));
-    } catch (error) {
-      next(error);
-    }
-  });
+  APP_ROUTER.post(
+    '/api/employees/',
+    VALIDATION_MIDDLEWARE(CreateEmployeeSchema),
+    async (req, res, next) => {
+      try {
+        return res.status(201).send(await EMPLOYEE.create(req.body));
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
 
-  APP_ROUTER.put('/api/employees/:id', async (req, res, next) => {
-    try {
-      return res
-        .status(204)
-        .send(await EMPLOYEE.update({ ...req.params, ...req.body }));
-    } catch (error) {
-      next(error);
-    }
-  });
+  APP_ROUTER.put(
+    '/api/employees/:id',
+    VALIDATION_MIDDLEWARE(UpdateEmployeeSchema),
+    async (req, res, next) => {
+      try {
+        return res
+          .status(204)
+          .send(await EMPLOYEE.update({ ...req.params, ...req.body }));
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
 
-  APP_ROUTER.delete('/api/employees/:id', async (req, res, next) => {
-    try {
-      return res.status(204).send(await EMPLOYEE.delete(req.params));
-    } catch (error) {
-      next(error);
-    }
-  });
+  APP_ROUTER.delete(
+    '/api/employees/:id',
+    VALIDATION_MIDDLEWARE(DeleteEmployeeSchema),
+    async (req, res, next) => {
+      try {
+        return res
+          .status(204)
+          .send(await EMPLOYEE.delete({ id: req.params.id }));
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
 
   return APP_ROUTER;
 };
